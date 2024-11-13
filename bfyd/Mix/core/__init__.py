9e126cea67399459a35b33ae9df486463727f801
bluefloyd24 <gluhdb@gmail.com>
2024-11-13 07:05:31 +0700

    Delete bfyd directory

--- bfyd/Mix/core/__init__.py
+++
@@ -1,9 +0,0 @@
-from .btn import *
-from .cek_update import *
-from .colong import humanbytes, progress, time_formatter
-from .git import *
-from .misc import *
-from .msgty import *
-from .newdb import *
-from .pastebin import *
-from .tools_anim import *
--- bfyd/modular/group.py
+++
@@ -1,4 +0,0 @@
-from Mix import get_cgr
-
-__modles__ = "Group"
-__help__ = get_cgr("help_group")
