ad7a4e57b7a9caf56572ed96a6d6b784dd68ddc3
bluefloyd24 <gluhdb@gmail.com>
2024-11-14 02:17:57 +0700

    Update call_payment.py

--- assistant/call_payment.py
+++ assistant/call_payment.py
@@ -10,11 +10,11 @@ from modular.payment import payment_message  # Pastikan ini sesuai dengan lokasi
 @ky.callback("payme.")
 async def _(c, cq):
     cmd = cq.data.split(".")[1]
-    kb = ikb([[("Kembali", "bace.payme")]])
+    kb = ikb([[("Kembali", "bek.payme")]])
     if cmd == "butformat":
         await cq.edit_message_text(text=payment_message, reply_markup=kb, parse_mode=ParseMode.HTML)
 
-@ky.callback("bace")
+@ky.callback("bek")
 async def _(c, cq):
     txt = cgr("payme_1")
     ke = ikb([[("Payment", "payme.butformat")]])
