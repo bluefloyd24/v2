76ce6ebd4e912e7221a8b3519877c26db7b04ffd
bluefloyd24 <gluhdb@gmail.com>
2024-11-14 02:04:35 +0700

    Update call_payment.py

--- assistant/call_payment.py
+++ assistant/call_payment.py
@@ -1,22 +1,21 @@
+# call_payment.py
 from pyrogram import *
 from pyrogram.enums import *
 from pyrogram.helpers import ikb
 from pyrogram.types import *
-
 from Mix import *
 
+from modular.payment import payment_message  # Pastikan ini sesuai dengan lokasi file
 
 @ky.callback("payme.")
 async def _(c, cq):
     cmd = cq.data.split(".")[1]
-    kb = ikb([[("Kembali", "bace.markd")]])
+    kb = ikb([[("Kembali", "bace.payme")]])
     if cmd == "butformat":
-        nan = cgr("mark_1")
-        await cq.edit_message_text(text=nan, reply_markup=kb, parse_mode=ParseMode.HTML)
-
+        await cq.edit_message_text(text=payment_message, reply_markup=kb, parse_mode=ParseMode.HTML)
 
 @ky.callback("bace")
 async def _(c, cq):
-    txt = cgr("mark_3")
+    txt = cgr("payme_1")
     ke = ikb([[("Payment", "payme.butformat")]])
     await cq.edit_message_text(text=txt, reply_markup=ke)
