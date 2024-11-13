2813b8ee7176863bc87868c4533fd4142d3a753c
bluefloyd24 <gluhdb@gmail.com>
2024-11-14 02:05:41 +0700

    Update payment.py

--- modular/payment.py
+++ modular/payment.py
@@ -1,11 +1,27 @@
+# payment.py
 from pyrogram.enums import *
 from pyrogram.types import *
-
 from Mix import *
 
 __modles__ = "Payment"
 __help__ = get_cgr("help_payme")
 
+# Variabel untuk menyimpan pesan dinamis pengguna
+payment_message = "Ini adalah channel pembayaran default. Silakan atur pesan dengan .setcgrpay1"
+
+@ky.ubot("setcgrpay1")
+async def set_payment_message(c, m):
+    global payment_message
+    args = m.text.split(maxsplit=1)
+    
+    # Cek apakah pengguna memasukkan pesan
+    if len(args) < 2:
+        await m.reply("Harap masukkan pesan. Contoh: `.setcgrpay1 ini adalah channel pembayaran saya t.me/bbluepay`")
+        return
+    
+    # Atur pesan pembayaran dengan teks baru
+    payment_message = args[1]
+    await m.reply("Pesan pembayaran berhasil diatur.")
 
 @ky.ubot("payment")
 async def _(c: nlx, m):
