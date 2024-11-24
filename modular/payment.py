164a3823dbc8153e6d26fb08899df12e02652c75
bluefloyd24 <gluhdb@gmail.com>
2024-11-25 05:39:23 +0700

    Update payment.py

--- modular/payment.py
+++ modular/payment.py
@@ -11,13 +11,14 @@ DEVS = [6878107336]
 
 @ky.ubot("payment")
 async def _(c: nlx, m):
+    em = Emojik()
+    em.initialize()
     user_id = m.from_user.id
     if user_id not in DEVS:
         await m.reply(cgr("payme_2").format(em.gagal))
         return
-        
-    em = Emojik()
-    em.initialize()
+
+    
     try:
         xi = await c.get_inline_bot_results(bot.me.username, "payme_in")
         await c.send_inline_bot_result(m.chat.id, xi.query_id, xi.results[0].id, reply_to_message_id=ReplyCheck(m))
