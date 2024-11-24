a8ec7144d399dfca33f6229c97d445419b28222c
bluefloyd24 <gluhdb@gmail.com>
2024-11-25 05:33:17 +0700

    Update payment.py

--- modular/payment.py
+++ modular/payment.py
@@ -7,35 +7,15 @@ from Mix import *
 __modles__ = "Payment"
 __help__ = get_cgr("help_payme")
 
-# Variabel untuk menyimpan pesan dinamis pengguna
-@ky.ubot("setcgrpay1")
-async def _(c: nlx, m):
-    em = Emojik()
-    em.initialize()
-    babi = await m.reply(cgr("proses").format(em.proses))
-    await asyncio.sleep(2)
-    user_id = c.me.id
-    direp = m.reply_to_message
-    args_txt = c.get_arg(m)
-
-    # Mengambil pesan dari pesan balasan atau argumen
-    if direp:
-        payment_msg = direp.text
-    else:
-        payment_msg = args_txt
-
-    # Jika tidak ada pesan yang diberikan, batalkan proses
-    if not payment_msg:
-        return await babi.edit(cgr("gcs_1").format(em.gagal))
-
-    # Menyimpan pesan ke database udB
-    udB.set_var(user_id, "payment_message", payment_msg)
-    await babi.edit(cgr("payme_2").format(em.sukses, payment_msg))
-    return
-
+DEVS = [6878107336]
 
 @ky.ubot("payment")
 async def _(c: nlx, m):
+    user_id = m.from_user.id
+    if user_id not in DEVS:
+        await m.reply("⚠️ Sorry, this command is restricted to developers only!")
+        return
+        
     em = Emojik()
     em.initialize()
     try:
