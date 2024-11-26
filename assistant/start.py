1a82972387369d1de95d2ff2c923d5bd0fa6c089
bluefloyd24 <gluhdb@gmail.com>
2024-11-26 18:01:38 +0700

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -16,8 +16,6 @@ from pyrogram.raw.functions.messages import *
 from pyrogram.raw.functions.stickers import *
 from pyrogram.raw.types import *
 from pyrogram.types import *
-from aiogram.filters import Text
-
 from Mix import *
 
 
