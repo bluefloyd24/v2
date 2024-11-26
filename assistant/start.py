75d3e463a7880b1de0e92d9c8bcc2ed529ebd49f
bluefloyd24 <gluhdb@gmail.com>
2024-11-26 18:47:53 +0700

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -16,6 +16,7 @@ from pyrogram.raw.functions.messages import *
 from pyrogram.raw.functions.stickers import *
 from pyrogram.raw.types import *
 from pyrogram.types import *
+from aiogram.filters import F
 from Mix import *
 
 
@@ -42,10 +43,7 @@ async def _(c, m):
     # Kirim pesan menggunakan InlineKeyboardMarkup
     await m.reply(ts_2, reply_markup=keyboard)
 
+@ky.bots(F.text == cgr("asst_9"))
+async def handle_asst_9(c, m):
+    await m.reply("Tombol 'asst_9' ditekan!")
 
-@ky.bots(None, human.pv)  # Tidak spesifik perintah, menangkap semua teks
-async def handle_button_click(c, m):
-    if m.text == cgr("asst_6"):
-        await m.reply("Tombol 'asst_6' ditekan! Menjalankan aksi reboot.")
-        os.execl(sys.executable, sys.executable, "-m", "Mix")
- 
