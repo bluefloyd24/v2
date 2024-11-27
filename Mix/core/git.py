b1ffd34d4d1d823b855b75285897bd06971eabce
bluefloyd24 <gluhdb@gmail.com>
2024-11-27 21:55:30 +0700

    Update git.py

--- Mix/core/git.py
+++ Mix/core/git.py
@@ -43,38 +43,29 @@ def git():
     if config.git_token:
         GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
         TEMP_REPO = REPO_LINK.split("https://")[1]
-        upstream_repo = f"https://{GIT_USERNAME}:{config.git_token}@{TEMP_REPO}"
+        UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.git_token}@{TEMP_REPO}"
     else:
         UPSTREAM_REPO = config.upstream_repo
+
     try:
-        repo = Repo()
-        LOGGER.info(f"Git Client Found [VPS DEPLOYER]")
-    except GitCommandError:
-        LOGGER.info(f"Invalid Git Command")
+        repo = Repo()  # Mendeteksi repository di direktori saat ini
+        LOGGER.info("Git repository found.")
     except InvalidGitRepositoryError:
-        repo = Repo.init()
-        if "origin" in repo.remotes:
-            origin = repo.remote("origin")
-        else:
-            origin = repo.create_remote("origin", UPSTREAM_REPO)
+        LOGGER.warning("No valid Git repository found. Initializing...")
+        repo = Repo.init("/app")  # Inisialisasi repository di direktori /app
+        origin = repo.create_remote("origin", UPSTREAM_REPO)
         origin.fetch()
-        repo.create_head(
-            config.upstream_branch,
-            origin.refs[config.upstream_branch],
-        )
-        repo.heads[config.upstream_branch].set_tracking_branch(
-            origin.refs[config.upstream_branch]
-        )
-        repo.heads[config.upstream_branch].checkout(True)
-        try:
-            repo.create_remote("origin", config.upstream_repo)
-        except BaseException:
-            pass
-        nrs = repo.remote("origin")
-        nrs.fetch(config.upstream_branch)
-        try:
-            nrs.pull(config.upstream_branch)
-        except GitCommandError:
-            repo.git.reset("--hard", "FETCH_HEAD")
-        install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGGER.info(f"Fetched Updates from: {REPO_LINK}")
+        repo.create_head(config.upstream_branch, origin.refs[config.upstream_branch])
+        repo.heads[config.upstream_branch].set_tracking_branch(origin.refs[config.upstream_branch])
+        repo.heads[config.upstream_branch].checkout()
+    except GitCommandError as e:
+        LOGGER.error(f"Git command error: {e}")
+        return
+
+    try:
+        origin = repo.remote("origin")
+        origin.pull(config.upstream_branch)
+        LOGGER.info(f"Fetched updates from: {UPSTREAM_REPO}")
+    except GitCommandError as e:
+        LOGGER.error(f"Failed to fetch updates: {e}")
+        repo.git.reset("--hard", "FETCH_HEAD")
