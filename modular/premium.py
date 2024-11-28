b39d0f8497bc6c35f91107f296768d12cfd4a17c
bluefloyd24 <gluhdb@gmail.com>
2024-11-28 10:10:15 +0700

    Update premium.py

--- modular/premium.py
+++ modular/premium.py
@@ -8,25 +8,27 @@ from Mix import *
 async def premium(c, m):
     em = Emojik()
     em.initialize()
-    
-    # Periksa apakah pengguna adalah developer
     if m.from_user.id not in DEVS:
         return await m.reply(cgr("prem_1").format(em.gagal))
     
-    # Pisahkan teks dan validasi argumen
-    args = m.text.split(maxsplit=2)  # Ambil hingga maksimal 3 bagian
+    args = m.text.split(maxsplit=2) 
     
     if len(args) < 2:
         return await m.reply(cgr("prem_2").format(em.gagal))
     
     try:
-        # Ambil hanya angka durasi
         duration = int(args[1])
     except ValueError:
         return await m.reply(cgr("prem_3").format(em.gagal))
     
-    # Set premium untuk pengguna
-    udB.set_premium(m.reply_to_message.from_user.id, duration)
-
-    await m.reply(cgr("prem_4").format(m.from_user.first_name, duration))
+    if not m.reply_to_message:
+        return await m.reply("❌ Harap balas pesan pengguna yang ingin diberi premium.")
+   
+    target_user = m.reply_to_message.from_user
+    target_id = target_user.id
+    target_mention = f"<a href='tg://user?id={target_id}'>{target_user.first_name}</a>"
+    
+    udB.set_premium(target_id, duration)
+    
+    await m.reply(cgr("prem_4").format(target_mention, duration), parse_mode="HTML")
 
